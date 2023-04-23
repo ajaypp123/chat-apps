148fd35639bd60594142429bf40a2a840eb3d63e
Ajay Paratmandali <ajaypp123@gmail.com>
2023-04-23 22:36:37 +0530

    Started on project

--- cmd/server/chat-apps.go
+++ cmd/server/chat-apps.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net/http"
 
+	pb "github.com/ajaypp123/chat-apps/internal/communication_grpc"
+	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedChatServiceServer
+}
+
+func (s *server) SendMessage(ctx context.Context, msg *pb.Meg) (*pb.Ack, error) {
+	// add your SendMessage logic here
+	return nil, nil
+}
+
+func main() {
+	// web connection
+	// create gRPC server
+	grpcServer := grpc.NewServer()
+
+	// register ChatService server to gRPC server
+	pb.RegisterChatServiceServer(grpcServer, &server{})
+
+	// create Gorilla Mux router
+	router := mux.NewRouter()
+
+	// attach gRPC server to Gorilla Mux server
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		grpcServer.ServeHTTP(w, r)
+	})
+
+	// start Gorilla Mux server
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
